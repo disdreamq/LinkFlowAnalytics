7fedaff984ac83b971e06e8a3d5a6909313bd1bf
disdreamq <wlad050303@yandex.ru>
2025-11-26 17:26:03 +0300

    fixed link model

--- src/entities/link/models.py
+++ src/entities/link/models.py
@@ -15,7 +15,7 @@ class Link(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
     base_url: Mapped[str] = mapped_column(nullable=False)
-    url: Mapped[str] = mapped_column(nullable=False)
+    url: Mapped[str] = mapped_column(nullable=False, unique=True)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now()
