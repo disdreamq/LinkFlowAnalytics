05a08cc144a04825cf3dea5571890c4fdb0c36bf
disdreamq <wlad050303@yandex.ru>
2025-11-28 16:24:23 +0300

    refactoring

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+import uvicorn
+from src.entities.link.router import router
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+
+app = FastAPI(lifespan=lifespan)
+app.include_router(router)
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
