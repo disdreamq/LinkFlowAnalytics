1246bff0d66f623b5279e7aa04dc698500848758
disdreamq <wlad050303@yandex.ru>
2025-12-01 17:03:37 +0300

    fixed reload dirs

--- main.py
+++ main.py
@@ -16,4 +16,10 @@ app.include_router(router)
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        "main:app",
+        host="0.0.0.0",
+        port=8000,
+        reload=True,
+        reload_dirs=["src", "main.py"],
+    )
