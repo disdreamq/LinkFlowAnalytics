6575075a7e4a28895c56e75986d8cc89e22d1470
disdreamq <wlad050303@yandex.ru>
2025-12-03 18:51:03 +0300

    added click_counter for link model

--- src/modules/link/models.py
+++ src/modules/link/models.py
@@ -11,7 +11,7 @@ class Link(Base):
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"))
     base_url: Mapped[str] = mapped_column(nullable=False)
     url: Mapped[str] = mapped_column(nullable=False, unique=True)
-    click_counter: Mapped[int] = mapped_column(default=0, server_default=0)
+    click_counter: Mapped[int] = mapped_column(default=0)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
     updated_at: Mapped[datetime] = mapped_column(
         server_default=func.now(), onupdate=func.now()
