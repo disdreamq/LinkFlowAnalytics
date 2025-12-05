76c856adc4cc57937f91a58df9b50297c695b6be
disdreamq <wlad050303@yandex.ru>
2025-12-05 17:03:12 +0300

    refactored schemas for premium user analytics

--- src/modules/analytics/premium_user/schemas.py
+++ src/modules/analytics/premium_user/schemas.py
@@ -1,15 +1,12 @@
+from src.modules.analytics.base_user.schemas import (
+    SBaseUserSingleLinkResponse,
+    SBaseUserAllLinksResponse,
+)
 
-from src.modules.analytics.base_user.schemas import SBaseUserLinkStatsResponse, SUserLinksRequest
 
-
-class SPremiumUserLinkStatsResponse(SBaseUserLinkStatsResponse):
+class SPremiumUserLinkStatsResponse(SBaseUserSingleLinkResponse):
     distribution_by_browser: dict[str, int]
 
 
-class SBaseUserLinksResponse(SUserLinksRequest):
-    full_distribution_by_click_counter: dict[str, int]
-    full_distribution_by_week_days: dict[str, int]
-
-
-class SPremiumUserLinksResponse(SBaseUserLinksResponse):
+class SPremiumUserLinksResponse(SBaseUserAllLinksResponse):
     full_distribution_by_browser: dict[str, int]
