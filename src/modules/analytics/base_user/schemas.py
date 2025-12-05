e4b974a2efb6aa0e511ef320ff587eb487660963
disdreamq <wlad050303@yandex.ru>
2025-12-05 15:16:01 +0300

    added schemas for base user analytics

---
+++ src/modules/analytics/base_user/schemas.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+
+
+class SLinkStatsRequest(BaseModel):
+    url: str
+
+
+class SUserLinksRequest(BaseModel):
+    user_id: int
+
+
+class SBaseUserLinkStatsResponse(SLinkStatsRequest):
+    click_counter: int
+    distribution_by_week_days: dict[str, int]
+
+
+class SBaseUserLinksResponse(SUserLinksRequest):
+    full_distribution_by_click_counter: dict[str, int]
+    full_distribution_by_week_days: dict[str, int]
