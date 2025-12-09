2a50b0a5be00909fd06516dd277034fca79a1fec
disdreamq <wlad050303@yandex.ru>
2025-12-09 18:07:17 +0300

    added routers to main router

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -3,6 +3,7 @@ from .link.router import router as link_router
 from .link.redirect_router import router as link_redirect_router
 from .user.router import router as user_router
 from .auth.router import router as auth_router
+from .analytics import router as analytics_router
 
 router = APIRouter()
 
@@ -10,6 +11,7 @@ router.include_router(auth_router)
 router.include_router(link_router)
 router.include_router(user_router)
 router.include_router(link_redirect_router)
+router.include_router(analytics_router)
 
 from .user.models import User
 from .link.models import Link
--- src/modules/analytics/__init__.py
+++ src/modules/analytics/__init__.py
@@ -4,5 +4,6 @@ from .base_user.router import router as base_user_router
 from .premium_user.router import router as premium_user_router
 
 router = APIRouter()
-router.include_router(base_user_router)
+
 router.include_router(premium_user_router)
+router.include_router(base_user_router)
