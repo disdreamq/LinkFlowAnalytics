5b70392ae44eccda32a388d854ffa9390d0f96ac
disdreamq <wlad050303@yandex.ru>
2025-12-15 17:21:19 +0300

    refactoring

--- src/modules/auth/router.py
+++ src/modules/auth/router.py
@@ -8,10 +8,8 @@ from src.modules.auth.schemas import STokenResponse
 from src.modules.auth.service import (
     authenticate_user,
     create_access_token,
-    get_current_user,
 )
 from src.modules.user.dependencies import get_user_service
-from src.modules.user.schemas.schemas import SUserInDB, SUserUpdate
 from src.modules.user.service import UserService
 
 router = APIRouter(tags=["auth"])
@@ -28,19 +26,3 @@ async def login_for_access_token(
         data={"sub": user.email}, expires_delta=access_token_expires
     )
     return access_token
-
-
-@router.get("/me")
-async def read_user_me(
-    current_user: Annotated[SUserInDB, Depends(get_current_user)],
-):
-    return current_user
-
-
-@router.put("/tarifplan")
-async def change_tarifplan(
-    user_to_update: SUserUpdate,
-    service: Annotated[UserService, Depends(get_user_service)],
-):
-    updated_user = await service.update_user(user_to_update)
-    return updated_user
