2d5e9b65608c2a72085745a46830fec6c3512fa8
disdreamq <wlad050303@yandex.ru>
2025-12-15 17:22:37 +0300

    refactored for id_check to improve security

--- src/modules/link/service/service.py
+++ src/modules/link/service/service.py
@@ -1,5 +1,6 @@
 from typing import Literal
 
+from src.core.exception_factory import exception_factory
 from src.modules.link.repository import LinkRepository
 from src.modules.link.schemas.schemas import (
     SLinkCreate,
@@ -23,21 +24,44 @@ class LinkService:
         )
         return SLinkResponse.model_validate(new_link)
 
-    async def get_link(self, url: str) -> SLinkResponse:
-        if await self.redis.exists(f"link_url_{url}"):
-            link_from_redis = await self.redis.get(f"link_url_{url}")
-            if not link_from_redis:
-                raise
-            link = SLinkResponse.model_validate_json(link_from_redis)
+    async def get_link(
+        self, user_id: int, link_url: str
+    ) -> SLinkResponse:
+        await self._verify_id(user_id, link_url)
+        if (link_in_cache := await self.redis.get(f"link_url_{link_url}")):
+            link_in_cache = await self.redis.get(f"link_url_{link_url}")
+            return SLinkResponse.model_validate(link_in_cache)
         else:
-            link_in_db = await self.repo.get_link_by_url(url)
+            link_in_db = await self.repo.get_link_by_url(link_url)
             link = SLinkResponse.model_validate(link_in_db)
-            await self.redis.set_(f"link_url_{url}", link.model_dump_json(), expire=10)
-        return link
+            await self.redis.set_(
+                f"link_url_{link_url}", link.model_dump_json(), expire=10
+            )
+            return link
 
-    async def get_link_with_clicks(self, link_url: str) -> SLinkWithClicksResponse:
+
+    async def get_link_with_clicks(
+        self, user_id: int, link_url: str
+    ) -> SLinkWithClicksResponse:
+        await self._verify_id(user_id, link_url)
         link_wtih_clicks = await self.repo.get_link_with_clicks(link_url)
         return SLinkWithClicksResponse.model_validate(link_wtih_clicks)
+
+    async def update_link(
+        self, user_id: int, link_to_update: SLinkUpdate
+    ) -> SLinkResponse:
+        await self._verify_id(user_id, link_to_update.url)
+        link_url = link_to_update.url
+        link_data = link_to_update.model_dump(
+            exclude_unset=True,
+            exclude_none=True,
+        )
+        updated_user = await self.repo.update_link(link_url, link_data)
+        return SLinkResponse.model_validate(updated_user)
+
+    async def delete_link(self, user_id: int, link_url: str) -> Literal[True]:
+        await self._verify_id(user_id, link_url)
+        return await self.repo.delete_link(link_url)
 
     async def increment_click_counters(
         self, links_data: dict[int, int]
@@ -50,14 +74,22 @@ class LinkService:
             for link in links_with_incemented_click_counter
         ]
 
-    async def update_link(self, link_to_update: SLinkUpdate) -> SLinkResponse:
-        link_url = link_to_update.url
-        link_data = link_to_update.model_dump(
-            exclude_unset=True,
-            exclude_none=True,
-        )
-        updated_user = await self.repo.update_link(link_url, link_data)
-        return SLinkResponse.model_validate(updated_user)
+    async def get_link_for_redirect(
+        self,
+        link_url: str,
+    ) -> SLinkResponse:
+        if (link_in_cache := await self.redis.get(f"link_url_{link_url}")):
+            link_in_cache = await self.redis.get(f"link_url_{link_url}")
+            return SLinkResponse.model_validate(link_in_cache)
+        else:
+            link_in_db = await self.repo.get_link_by_url(link_url)
+            link = SLinkResponse.model_validate(link_in_db)
+            await self.redis.set_(
+                f"link_url_{link_url}", link.model_dump_json(), expire=10
+            )
+            return link
 
-    async def delete_link(self, link_url: str) -> Literal[True]:
-        return await self.repo.delete_link(link_url)
+    async def _verify_id(self, current_user_id: int, link_url: str):
+        link = await self.repo.get_link_by_url(link_url)
+        if current_user_id != link.user_id:
+            raise exception_factory.not_found("link id", "{link.id}")
