ede36fe23874816335fa217b06a83ff999bee4ef
disdreamq <wlad050303@yandex.ru>
2025-12-15 17:20:24 +0300

    deleted useless check_id, moved it to service

--- src/modules/analytics/base_user/router.py
+++ src/modules/analytics/base_user/router.py
@@ -9,12 +9,11 @@ from src.modules.analytics.base_user.schemas import (
     SBaseUserSingleLinkResponse,
 )
 from src.modules.analytics.base_user.service import (
-    check_id,
     get_distribution_by_week_days,
     get_full_distribution_by_click_counter_for_user,
     get_full_distribution_by_week_days_for_user,
 )
-from src.modules.auth.service import get_current_user
+from src.modules.auth.dependencies import require_auth
 from src.modules.link.dependencies import get_link_service
 from src.modules.link.service.service import LinkService
 from src.modules.user.dependencies import get_user_service
@@ -37,7 +36,7 @@ router = APIRouter(prefix="/analytics", tags=["users"])
     },
 )
 async def get_full_links_analytics(
-    current_user: Annotated[SUserInDB, Depends(get_current_user)],
+    current_user: Annotated[SUserInDB, Depends(require_auth)],
     link_service: Annotated[LinkService, Depends(get_link_service)],
     user_service: Annotated[UserService, Depends(get_user_service)],
 ):
@@ -67,12 +66,13 @@ async def get_full_links_analytics(
 )
 async def get_analytics_for_link(
     link_url: str,
-    current_user: Annotated[SUserInDB, Depends(get_current_user)],
+    current_user: Annotated[SUserInDB, Depends(require_auth)],
     link_service: Annotated[LinkService, Depends(get_link_service)],
 ):
-    await check_id(link_url, current_user.id, link_service)
     click_counter = (await link_service.get_link_for_redirect(link_url)).click_counter
-    distr_by_week_days = await get_distribution_by_week_days(link_url, link_service)
+    distr_by_week_days = await get_distribution_by_week_days(
+        user_id=current_user.id, link_url=link_url, service=link_service
+    )
     return SBaseUserSingleLinkResponse(
         url=link_url,
         click_counter=click_counter,
