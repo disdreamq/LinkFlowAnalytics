822b7fe3af1684e9edb818c3ba24cea3b6c55ff0
disdreamq <wlad050303@yandex.ru>
2025-12-22 16:18:54 +0300

    added user abstract repo

---
+++ src/modules/user/repositories/abstract_repositories.py
@@ -0,0 +1,19 @@
+from abc import abstractmethod
+
+from src.modules.abstract_repository import ICRUDRepository
+
+
+class IORMUserRepository[T](ICRUDRepository):
+    """
+    Abstract ORM user repository for ORMs with eager load like SQLAlchemy.
+
+    Raises exception if link not found, return user model with relationship if needed.
+    """
+
+    @abstractmethod
+    async def get_by_email(self, email: str) -> T:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_with_links(self, user_id: int) -> T:
+        raise NotImplementedError
