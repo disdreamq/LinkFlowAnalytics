3d37f77ebe95bfc152e3daf71e40e778729983dd
disdreamq <wlad050303@yandex.ru>
2025-12-22 16:18:44 +0300

    added link abstract repo

---
+++ src/modules/link/repositories/abstract_repositories.py
@@ -0,0 +1,31 @@
+from abc import abstractmethod
+
+from src.modules.abstract_repository import ICRUDRepository
+
+
+class IORMLinkRepository[T](ICRUDRepository):
+    """
+    Abstract ORM link repository for ORMs with eager load like SQLAlchemy.
+
+    Raises exception if link not found, return link model with relationship if needed.
+    """
+
+    @abstractmethod
+    async def get_by_url(self, url: str) -> T:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_with_clicks(self, url: str) -> T:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_multiple_links_by_urls(self, urls: list[str]) -> list[T]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def get_multiple_links_by_ids(self, link_ids: list[int]) -> list[T]:
+        raise NotImplementedError
+
+    @abstractmethod
+    async def increment_click_counters(self, links_data: dict[int, int]) -> list[T]:
+        raise NotImplementedError
