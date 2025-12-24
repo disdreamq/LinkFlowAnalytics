fbb050ff8cba41467b41caaa94dd525651106f61
disdreamq <wlad050303@yandex.ru>
2025-12-24 16:04:29 +0300

    refactoring

---
+++ src/cache/redis/repositories/abstract_repository.py
@@ -0,0 +1,14 @@
+from abc import abstractmethod
+from typing import Any
+
+from core.abstract_repositories.key_value_repository import IKeyValueRepository
+
+
+class IRedisRepository(IKeyValueRepository):
+    @abstractmethod
+    async def add_to_arr(self, *entity_data: Any) -> int:
+        raise NotImplementedError from None
+
+    @abstractmethod
+    async def get_arr(self, *entity_data: Any) -> list:
+        raise NotImplementedError from None
