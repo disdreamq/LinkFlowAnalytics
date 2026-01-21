e767898abbd8472844ac6f04c617314e18fc92f2
disdreamq <wlad050303@yandex.ru>
2026-01-21 16:36:36 +0300

    fixed bug url gen was starting with index 1 instead of 0

--- src/modules/link/service/url_generator.py
+++ src/modules/link/service/url_generator.py
@@ -17,20 +17,23 @@ class URLGenerator:
         self.ready = False
 
     def _generate_url(self):
-        for i in range(4, 0, -1):
-            if self.current[i] == len(self.alphabet) - 1:
+        for i in range(4, -1, -1):
+            if self.current[i] == len(self.alphabet):
                 self.current[i] = 0
                 self.current[i - 1] += 1
-        url: str = "".join([self.alphabet[index] for index in self.current])
 
+        url: str = "".join([self.alphabet[index] for index in self.current])
         self.current[-1] += 1
+
         yield url
 
     async def _initialize(self):
         cached_current = await self.cache.get("current")
         if cached_current:
             self.current = [int(elem) for elem in cached_current.split(",")]
-        self.current[4] += 1
+            self.current[4] += 1
+        else:
+            self.current = [0, 0, 0, 0, 0]
         self.generator = self._generate_url
         self.ready = True
 
@@ -42,4 +45,5 @@ class URLGenerator:
         return next(self.generator())
 
     async def del_cache(self):
-        return await self.cache.delete("current")
+        await self.cache.delete("current")
+        self.ready = False
