582ef64580c5543e8b6c5a219eae7374b139f31f
disdreamq <wlad050303@yandex.ru>
2026-01-24 14:45:01 +0300

    added dependency for premium user analytics service

--- src/modules/analytics/premium_user/dependencies.py
+++ src/modules/analytics/premium_user/dependencies.py
@@ -4,8 +4,13 @@ from fastapi.params import Depends
 
 from src.core.exceptions.exceptions import PremissonDenaiedException
 from src.enums.enums import UserTarifPlan
+from src.modules.analytics.premium_user.service import PremiumUserAnalyticService
 from src.modules.auth.dependencies import require_auth
+from src.modules.link.dependencies import get_link_service
+from src.modules.link.service.service import LinkService
+from src.modules.user.dependencies import get_user_service
 from src.modules.user.schemas import SUserInDB
+from src.modules.user.service import UserService
 
 
 async def require_premimum_tarifplan(
@@ -15,3 +20,10 @@ async def require_premimum_tarifplan(
         raise PremissonDenaiedException(
             f"User with user id {current_user.id} doest not match required tarif plan"
         )
+
+
+async def get_premium_user_analytic_service(
+    link_service: Annotated[LinkService, Depends(get_link_service)],
+    user_service: Annotated[UserService, Depends(get_user_service)],
+) -> PremiumUserAnalyticService:
+    return PremiumUserAnalyticService(link_service, user_service)
