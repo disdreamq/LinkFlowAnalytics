27461b95b7fb858f6a9cac352038e14b84597461
disdreamq <wlad050303@yandex.ru>
2026-01-24 14:15:41 +0300

    refactored base user analytics router useing dependency for base user service dependency

--- src/modules/analytics/base_user/router.py
+++ src/modules/analytics/base_user/router.py
@@ -8,17 +8,10 @@ from src.modules.analytics.base_user.schemas import (
     SBaseUserAllLinksResponse,
     SBaseUserSingleLinkResponse,
 )
-from src.modules.analytics.base_user.service import (
-    get_distribution_by_week_days_single_link,
-    get_full_distribution_by_click_counter_for_user,
-    get_full_distribution_by_week_days_for_user,
-)
+from src.modules.analytics.base_user.service import BaseUserAnalyticService
+from src.modules.analytics.dependencies import get_base_user_analytics_service
 from src.modules.auth.dependencies import require_auth
-from src.modules.link.dependencies import get_link_service
-from src.modules.link.service.service import LinkService
-from src.modules.user.dependencies import get_user_service
 from src.modules.user.schemas import SUserInDB
-from src.modules.user.service import UserService
 
 logger = logging.getLogger(__name__)
 router = APIRouter(prefix="/analytics", tags=["users"])
@@ -37,14 +30,19 @@ router = APIRouter(prefix="/analytics", tags=["users"])
 )
 async def get_full_links_analytics(
     current_user: Annotated[SUserInDB, Depends(require_auth)],
-    link_service: Annotated[LinkService, Depends(get_link_service)],
-    user_service: Annotated[UserService, Depends(get_user_service)],
+    analytic_service: Annotated[
+        BaseUserAnalyticService, Depends(get_base_user_analytics_service)
+    ],
 ):
-    distr_for_click_counter = await get_full_distribution_by_click_counter_for_user(
-        current_user.id, user_service
+    distr_for_click_counter = (
+        await analytic_service.get_full_distribution_by_click_counter_for_user(
+            current_user.id
+        )
     )
-    distr_by_week_days = await get_full_distribution_by_week_days_for_user(
-        current_user.id, link_service, user_service
+    distr_by_week_days = (
+        await analytic_service.get_full_distribution_by_week_days_for_user(
+            current_user.id
+        )
     )
     return SBaseUserAllLinksResponse(
         user_id=current_user.id,
@@ -67,14 +65,17 @@ async def get_full_links_analytics(
 async def get_analytics_for_link(
     link_url: str,
     current_user: Annotated[SUserInDB, Depends(require_auth)],
-    link_service: Annotated[LinkService, Depends(get_link_service)],
+    analytic_service: Annotated[
+        BaseUserAnalyticService, Depends(get_base_user_analytics_service)
+    ],
 ):
-    click_counter = (await link_service.get_for_redirect(link_url)).click_counter
-    distr_by_week_days = await get_distribution_by_week_days_single_link(
-        user_id=current_user.id, link_url=link_url, service=link_service
+    distr_by_week_days = (
+        await analytic_service.get_distribution_by_week_days_single_link(
+            user_id=current_user.id, link_url=link_url
+        )
     )
     return SBaseUserSingleLinkResponse(
         url=link_url,
-        click_counter=click_counter,
-        distribution_by_week_days=distr_by_week_days,
+        click_counter=distr_by_week_days[0],
+        distribution_by_week_days=distr_by_week_days[1],
     )
