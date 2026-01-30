2795ac490919c3d13efb04263a23572c9ea58b03
disdreamq <wlad050303@yandex.ru>
2026-01-30 17:27:30 +0300

    refactoring

--- tests/integration/user_router/conftest.py
+++ tests/integration/user_router/conftest.py
@@ -21,25 +21,3 @@ def sample_user_update_data():
         "tarifplan": UserTarifPlan.Premium,
     }
     return user_data
-
-
-@pytest.fixture(autouse=True)
-async def register_admin_user(client):
-    admin_data = {"email": "admin@example.com", "password": "adminadmin"}
-    await client.post(
-        "/users/",
-        json=admin_data,
-    )
-
-
-@pytest.fixture()
-async def admin_user(client):
-    admin_data = {"email": "admin@example.com", "password": "adminadmin"}
-    response = await client.post(
-        "/token",
-        data={"username": admin_data["email"], "password": admin_data["password"]},
-    )
-    token = response.json()["access_token"]
-    header = {"Authorization": f"Bearer {token}"}
-    resp = await client.get("users/1", headers=header)
-    return {"data": resp.json(), "header": header}
